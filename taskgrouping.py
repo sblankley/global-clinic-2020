d4d58d6437c7167c5b5f693323e628093f13d3ad
Steph Blankley <sblankley@hmc.edu>
2020-02-25 18:55:50 -0800

    fixed errors yay

--- taskgrouping.py
+++ taskgrouping.py
@@ -16,25 +16,25 @@ jobIsComputer = [0,1,0]
 #gives you the indices of human jobs in an array
 def humanJobs(jobIsComputer):
     humanj = []
-    for i in range(len(jobIsComputer)): 
+    for i in range(len(jobIsComputer)):
         #checks to see if job is a human job
         if jobIsComputer[i] == 0:     #if it is a human job, add it to humanj array
             humanj += [i]
         else:                         #if it is not a human job, don't change anything about humanj array
-            humanj = humanj 
-    return humanj 
+            humanj = humanj
+    return humanj
 
 #gives you the cycle times corresponding to human jobs
-def cycleTimehuman(jobIsComputer,cycleTime): 
+def cycleTimehuman(jobIsComputer,cycleTime):
     humanindex = humanJobs(jobIsComputer)          #runs humanJobs to get indices of human jobs
-    cycletimesH = []                            
+    cycletimesH = []
     for i in range(len(humanindex)):
-        cycletimesH += [cycleTime[humanindex[i]]]   #add the cycle time of human jobs to empty array 
+        cycletimesH += [cycleTime[humanindex[i]]]   #add the cycle time of human jobs to empty array
     return(cycletimesH)
 
-humanCap = max(cycleTimehuman(jobIsComputer,cycleTime))/takt  # maximum number of human workers allowed at each workstation 
+humanCap = max(cycleTimehuman(jobIsComputer,cycleTime))/takt  # maximum number of human workers allowed at each workstation
                                                               # is equal to number of human workers
-                                                              # needed to complete the longest human task 
+                                                              # needed to complete the longest human task
 
 pred = [[0, 1, 0],
         [0, 0, 1],
@@ -101,21 +101,28 @@ for w in range(workers):
 # each workstation has enough operators to complete assigned jobs under takt time
 for s in range(stations):
     m.Equation(sum(ifJobAtStation[j,s]*cycleTime[j] for j in range(jobs)) <=takt*numWorkersAssigned[s])
+# link workers at stations
+for s in range(stations):
+    m.Equation(sum(ifWorkerAtStation[w,s] for w in range(workers)) == numWorkersAssigned[s])
 # qualified workers
 for w in range(workers):
     for j in range(jobs):
         m.Equation(ifWorkerAtJob[w,j] <= ifQual[w][j])
 # keep from overcrowding
 for s in range(stations):
-    m.Equation(sum(sum(x[w,j,s] * (1-jobIsComputer[j]) for j in range(jobs)) for w in range(workers)) <= humanCap)
+    m.Equation(sum(sum(x[w,j,s] * (1-jobIsComputer[j]) for j in range(jobs)) for w in range(workers)) <= 2)
 
 for j1 in range(jobs):
     for j2 in range(jobs):
         for s2 in range(stations):
-            m.Equation(ifJobAtStation[j2,s2]*pred[j1][j2] <= sum(ifJobAtStation[j1,s1] for s1 in range(s2+1)))
+            m.Equation(ifJobAtStation[j2,s2]*pred[j1][j2] <= sum(ifJobAtStation[j1,s1] for s1 in range(s2)))
 
 # SOLVE
 m.options.SOLVER=1    # change solver (1=APOPT,3=IPOPT)
 m.solve(disp=True)
+print('humanCap:'+str(humanCap))
 print('x:'+str(x))
+print('ifJobAtStation:'+str(ifJobAtStation))
+print('ifWorkerAtStation:'+str(ifWorkerAtStation))
+print('ifWorkerAtJob:'+str(ifWorkerAtJob))
 print('numWorkersAssigned:'+str(numWorkersAssigned))
