66a996527f0c1c09b030c2d07e0b5f2545676af9
Steph Blankley <sblankley@hmc.edu>
2020-04-12 13:11:46 -0700

    placement holds l and w, need to add coords

--- stationPlacement.py
+++ stationPlacement.py
@@ -1,29 +1,37 @@
 # from csvReader import * # input data
-from taskgrouping import * 
+from taskgrouping import *
 # import results csv
 
+# dict with new stations as keys and jobs as values
 tasks =	{}
 for i in range(len(real_stations)):
 	tasks[i] = task_dist[i]
 
 operators = {}
-for i in range(len(real_stations)): # this should actually be indexed over types
+for i in range(len(real_stations)):
     operators[i] = op_dist[i]
 
-types  #original dictionary with types as keys as jobs as values from csv reader
+jobs  #original dictionary with types as keys as jobs as values from csv reader
 
-#uncomment these lines for CSVs with size data 
+assignedJobs = [[[] for k in types] for i in range(len(real_stations))]
+for s in range(len(real_stations)):
+	for t in types:
+		for j in jobs[t]:
+			if (j in tasks[s]):
+				assignedJobs[s][t].append(j)
+
+# assignedJobs = {{}}
+# for s in range(len(real_stations)):
+# 	for t in types:
+# 		temp = []
+# 		for j in jobs[t]:
+# 			if (j in tasks[s]):
+# 				temp.append(j)
+# 		assignedJobs[s][t] = temp
+
+#uncomment these lines for CSVs with size data
 length # dict with jobs as keys as length as values, from csv reader
 width # dict with jobs as keys and widths as values, from csv reader
-
-saved
- 
-
-# NEED THE FOLLOWING:
-# dict with jobs as keys as length as values
-# dict with jobs as keys and widths as values
-# dict with new stations as keys and jobs as values
-# dict with type as keys and jobs as values
 
 # results are indexed by station and have job # and number of each type of operator
 # we want to calculate the length and width of each station
@@ -31,20 +39,33 @@ saved
 # if it's a human type, double the width and multiply the length by (# ops / 2)
 # some computer jobs MUST be all next to each other -- will contemplate that
 
+placement = [[] for i in range(len(real_stations))]
 # for each newstation
 for s in range(len(real_stations)):
     # initialize temp demensions
-    temp = [] 
+    temp = []
     # for each type
-    #for t in range(len(op_dist[0])):
-        # get max length and width of the assigned tasks
-        
+    for t in types:
+		maxLength = 0
+		maxWidth = 0
+		for j in assignedJobs[s][t]:
+			if (length[j] > maxLength):
+				maxLength = length[j]
+			if (width[j] > maxWidth):
+				maxWidth = width[j]
         # if number of operators is greater than one
+		if (op_dist[s][t] > 1):
             # take number of operators and divide by two
             # temp will add that # times the length total length, and twice the width
+			temp[0] += ceil(op_dist[s][t]/2)*maxLength
+			temp[1] += 2*maxWidth
         # else (if number of operators is one or zero)
+		else:
             # temp will add that length and that width
+			temp[0] += maxLength
+			temp[1] += maxWidth
     # station gets dimensions of temp
+	placement[s] = temp
 
 # now we also want the bottom left corner of each new station in a line
 # assigne a buffer space between stations
